022bfe81f58dc26935c7f55e3ad3a70fceef28c9
KWilson718 <wilsonk18@spu.edu>
2025-01-20 17:25:07 -0800

    finalized comments on algorithms class

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,11 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Algorithm Execution Time Comparison Class Implementation
+// By Kyle Wilson
+// 
+// The stated purpose of this class is to compare the execution time of four algorithms
+//  - Each algorithm works to compute a provided base that is then multiplied to the power of a provided exponent
+//  - Four public variables can then be accessed with the execution times for further analysis
+
+// Include for header file
 #include "algorithms.h"
 
+// Include for time keeping library
 #include <chrono>
 
+// STD method declarations
 using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::nanoseconds;
 
+// Basic constructor declaration, sets everything to the correct form of zero
 Algorithms::Algorithms() {
     this->_doubleIterativeTime = 0.0;
     this->_doubleRecursiveTime = 0.0;
@@ -13,6 +26,7 @@ Algorithms::Algorithms() {
     this->_integerRecursiveTime = 0;
 }
 
+// Runs helper functions to calculate the time of each algorithm when running to the power of a provided exponent
 void Algorithms::findTimes(long long exponent) {
     
     calculateDoubleIterative(_DOUBLE_BASE, exponent);
@@ -25,6 +39,7 @@ void Algorithms::findTimes(long long exponent) {
 
 }
 
+// Resets each time to zero to prevent confusion
 void Algorithms::clearTimes() {
     this->_doubleIterativeTime = 0.0;
     this->_doubleRecursiveTime = 0.0;
@@ -32,46 +47,67 @@ void Algorithms::clearTimes() {
     this->_integerRecursiveTime = 0;
 }
 
+// Function used to wrap an algorithm & report out execution time to member variable
 void Algorithms::calculateDoubleIterative(long double base, long long exponent) {
+    // Starts clock
     auto start = high_resolution_clock::now();
 
+    // Executes algorithm
     long double tmpVal = doubleIterativeAlg(base, exponent);
 
+    // Ends clock
     auto end = high_resolution_clock::now();
 
+    // Sets appropriate variable to the time difference in nanoseconds
     this->_doubleIterativeTime = duration_cast<nanoseconds>(end - start).count();
 }
 
+// Function used to wrap an algorithm & report out execution time to member variable
 void Algorithms::calculateDoubleRecursive(long double base, long long exponent) {
+    // Starts clock
     auto start = high_resolution_clock::now();
 
+    // Executes algorithm
     long double tmpVal = doubleRecursiveAlg(base, exponent);
 
+    // Ends clock
     auto end = high_resolution_clock::now();
 
+    // Sets appropriate variable to the time difference in nanoseconds
     this->_doubleRecursiveTime = duration_cast<nanoseconds>(end - start).count();
 }
 
+// Function used to wrap an algorithm & report out execution time to member variable
 void Algorithms::calculateIntegerIterative(long long base, long long exponent) {
+    // Starts clock
     auto start = high_resolution_clock::now();
 
+    // Executes algorithm
     long long tmpVal = integerIterativeAlg(base, exponent);
 
+    // Ends clock
     auto end = high_resolution_clock::now();
 
+    // Sets appropriate variable to the time difference in nanoseconds
     this->_integerIterativeTime = duration_cast<nanoseconds>(end - start).count();
 }
 
+// Function used to wrap an algorithm & report out execution time to member variable
 void Algorithms::calculateIntegerRecursive(long long base, long long exponent) {
+    // Starts clock
     auto start = high_resolution_clock::now();
 
+    // Executes algorithm
     long long tmpVal = integerRecursiveAlg(base, exponent);
 
+    // Ends clock
     auto end = high_resolution_clock::now();
 
+    // Sets appropriate variable to the time difference in nanoseconds
     this->_integerRecursiveTime = duration_cast<nanoseconds>(end - start).count();
 }
 
+// Provided algorithm to analyze - Iterative Design using Doubles
 long double Algorithms::doubleIterativeAlg(long double base, long long exponent) {
     long double retVal = 1.0;
 	if (exponent < 0) {
@@ -84,6 +120,7 @@ long double Algorithms::doubleIterativeAlg(long double base, long long exponent)
 	return retVal;
 }
 
+// Provided algorithm to analyze - Recursive Design using Doubles
 long double Algorithms::doubleRecursiveAlg(long double base, long long exponent) {
     if (exponent < 0) {
 		return 1.0 / doubleRecursiveAlg(base, -exponent);
@@ -95,6 +132,7 @@ long double Algorithms::doubleRecursiveAlg(long double base, long long exponent)
 
 }
 
+// Provided algorithm to analyze - Iterative Design mutated to accept Integers
 long long Algorithms::integerIterativeAlg(long long base, long long exponent) {
     long long retVal = 1;
     if (exponent < 0) {
@@ -107,6 +145,7 @@ long long Algorithms::integerIterativeAlg(long long base, long long exponent) {
     return retVal;
 }
 
+// Provided algorithm to analyze - Recursive Design mutated to accept Integers
 long long Algorithms::integerRecursiveAlg(long long base, long long exponent) {
     if (exponent < 0) {
         return 1 / integerRecursiveAlg(base, -exponent);
